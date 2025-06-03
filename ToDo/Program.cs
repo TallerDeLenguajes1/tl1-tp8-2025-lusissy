ae849fae6edc21d7988be7e1f23dc72dd1a7cd13
lusissy <lourdessissy@gmail.com>
2025-06-03 12:57:39 -0300

    1.2

---
+++ ToDo/Program.cs
@@ -0,0 +1,16 @@
+﻿using ToDoEspacio;
+Random rnd = new Random();
+int numeroTareas=rnd.Next(3);
+int indice, duracion;
+string [] descripcion={"Limpiar", "Correr", "Estudiar", "Lavar"};
+
+List<Tarea> tPendientes= new List<Tarea>();
+List<Tarea> tRealizadas = new List<Tarea>();
+
+for (int i = 0; i < numeroTareas; i++)
+{
+    indice= rnd.Next(0,3);
+    duracion=rnd.Next(10,100);
+    Tarea pendiente= new Tarea(1000+i, descripcion[indice], duracion);
+    tPendientes.Add(pendiente);
+}
---
+++ ToDo/Tarea.cs
@@ -0,0 +1,16 @@
+namespace ToDoEspacio
+{
+    public class Tarea
+    {
+        public int TareaID { get; set; }
+        public string? Descripcion { get; set; }
+        public int Duracion { get; set; } 
+        public Tarea(int id, string desc, int duracion)
+        {
+            TareaID=id;
+            Descripcion=desc;
+            Duracion=duracion;
+        }
+    }
+    
+}
